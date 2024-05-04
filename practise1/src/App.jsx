13ec5ce8d884f22cc5bb4f7913113ad40aaeb940
prakhar <workprakhar@gmail.com>
2024-05-05 01:12:59 +0530

    Click_Count_Increment

---
+++ practise1/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count+1);
+  }
+
+  return(
+    <>
+      <div>
+        <button onClick={incrementCount}>Increment</button>
+        <h2> Click Count is : {count}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
