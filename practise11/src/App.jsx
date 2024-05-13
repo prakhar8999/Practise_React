eab3ec95df1754f2eca506ea6b13ae196737c86c
prakhar <workprakhar@gmail.com>
2024-05-13 23:56:32 +0530

    samsung api call push

--- practise11/src/App.jsx
+++ practise11/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [employeeData, setemployeeData] = useState([])
+  const [employeeName,setEmployeeName] = useState('')
+
+  const EmployeeNameSearch = (e) => {
+    setEmployeeName(e.target.value)
+
+  }
+
+  useEffect(() => {
+    fetch('https://mocki.io/v1/b543fcfa-4dcf-4f65-84fc-8e9e1c2c8aeb')
+    .then(response => response.json())
+    .then(edata => setemployeeData(edata.data))
+},[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input type='text' value={employeeName} 
+    onChange={EmployeeNameSearch}/>
+      {/* <ul>
+        {employeeData.map((employee) => (
+          <li key={employee.id}>{employee.firstName}</li>
+        ))}
+      </ul> */}
+      <ul>
+        {(employeeData.filter((employee) =>
+        employee.firstName.includes(employeeName) || employee.lastName.includes(employeeName))).map((data) => (<li key={data.id}>{data.firstName}</li>))}
+      </ul>
     </>
   )
 }
